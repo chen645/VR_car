8becfd4378a3181fcae7c86bd7d123c289706126
chen645 <53135189+chen645@users.noreply.github.com>
2021-06-19 18:49:47 +0800

    Add files via upload

---
+++ Rotate.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+public class Rotate : MonoBehaviour
+{
+    public Slider slider;
+    public Slider slider2;
+    public GameObject model;
+    // Start is called before the first frame update
+
+    public void sliderRotatemodel()
+    {
+        model.transform.rotation = Quaternion.Euler(model.transform.rotation.x, slider.value, model.transform.rotation.z);
+
+
+    }
+    
+}
